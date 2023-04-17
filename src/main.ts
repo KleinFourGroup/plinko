6580954a2cb39ef04b2d07665b0ed8de2734c7bd
Matthew Kilgore <m.kilgore0@gmail.com>
2023-04-17 11:02:42 -0400

    Split the falling orb into its own class.  Fixed bugged FPS counter. (Pixi gives delta in terms of frames rather than time.)  Added workload per frame tracker.

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,42 @@ let app = new PIXI.Application({ resizeTo: window, background: '#1099bb' });
 // @ts-ignore
 document.body.appendChild(app.view);
 
-let intervals: Array<any> = []
+class Orb {
+    world: Matter.World
+    body: Matter.Body
+    graphics: PIXI.Graphics
+    radius: number
+    x: number
+    y: number
+
+    constructor(world: Matter.World, radius: number, x: number, y: number) {
+        this.world = world
+        this.radius = radius
+        this.x = x
+        this.y = y
+
+        this.body = Matter.Bodies.circle(x, y, radius)
+        Matter.World.addBody(this.world, this.body)
+
+        this.graphics = new PIXI.Graphics()
+        this.graphics.beginFill(0x00FF00)
+        this.graphics.drawCircle(0, 0, this.radius)
+        this.graphics.endFill()
+
+        this.graphics.position.set(this.x, this.y)
+    }
+
+    addTo(parent: PIXI.Container) {
+        parent.addChild(this.graphics)
+    }
+
+    update() {
+        this.x = this.body.position.x
+        this.y = this.body.position.y
+        this.graphics.position.set(this.x, this.y)
+    }
+}
+
 let FPS = new PIXI.Text()
 FPS.position.set(5, 5)
 app.stage.addChild(FPS)
@@ -17,40 +52,62 @@ let world = engine.world
 let slab = Matter.Bodies.rectangle(300, 300, 500, 25, {isStatic: true})
 Matter.World.add(world, slab)
 
-let orb = Matter.Bodies.circle(300, 50, 20, {density: 1})
-Matter.World.add(world, orb)
-
 let rect = new PIXI.Graphics()
 rect.beginFill(0xFF0000)
 rect.drawRect(0, 0, 500, 25)
 rect.endFill()
 rect.pivot.set(250, 12.5)
 
-let circle = new PIXI.Graphics()
-circle.beginFill(0x00FF00)
-circle.drawCircle(0, 0, 20)
-circle.endFill()
-
 app.stage.addChild(rect)
-app.stage.addChild(circle)
+
+let orb = new Orb(world, 20, 300, 50)
+orb.addTo(app.stage)
 
 // Create the sprite and add it to the stage
 let sprite = new PIXI.Text("@") // PIXI.Sprite.from('sample.png');
-app.stage.addChild(sprite);
+app.stage.addChild(sprite)
+
+
+let intervalsTotal: Array<number> = []
+let intervalsWork: Array<number> = []
+
+let lastWork = 0
 
 // Add a ticker callback to move the sprite back and forth
+let lastStep = 0
 let elapsed = 0.0;
-app.ticker.add((delta) => {
-    console.log(Math.round(delta))
-    intervals.push(delta)
-    if (intervals.length > 5) intervals.shift()
-    FPS.text = Math.round((1000 * intervals.length) / intervals.reduce((a, b) => a + b, 0))
 
-    Matter.Engine.update(engine, 20)
+// delta is in frames, not ms =()
+function update(delta: number) {
+    let deltaMS: number = app.ticker.deltaMS
+
+    intervalsTotal.push(deltaMS)
+    intervalsWork.push(lastWork)
+
+    if (intervalsTotal.length > 20) {
+        intervalsTotal.shift()
+        intervalsWork.shift()
+    }
+
+    FPS.text = `${Math.round(app.ticker.FPS)} - ${Math.round((intervalsWork.reduce((a, b) => a + b, 0) / intervalsTotal.reduce((a, b) => a + b, 0) * 100))}%` 
+    
+    elapsed += deltaMS;
+
+    let startWork = performance.now()
+
+    if (elapsed - lastStep >= 20) {
+        lastStep = Math.floor(elapsed)
+        Matter.Engine.update(engine, 20)
+    }
 
     rect.position.set(slab.position.x, slab.position.y)
-    circle.position.set(orb.position.x, orb.position.y)
+    orb.update()
 
-    elapsed += delta;
     sprite.x = 100.0 + Math.cos(elapsed/50.0) * 100.0;
-});
+    
+    let endWork = performance.now()
+
+    lastWork = endWork - startWork
+}
+
+app.ticker.add(update);
