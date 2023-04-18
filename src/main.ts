5483515f85edfdc491e2fce8e0d66a5dd39f2fbd
Matthew Kilgore <m.kilgore0@gmail.com>
2023-04-18 10:29:26 -0400

    Made more robust classes for the two physics objects.  Reworked logic so a new orb spawns every second, up to ten total.

--- src/main.ts
+++ src/main.ts
@@ -6,29 +6,27 @@ let app = new PIXI.Application({ resizeTo: window, background: '#1099bb' });
 // @ts-ignore
 document.body.appendChild(app.view);
 
-class Orb {
+class PhysicsObject {
     world: Matter.World
     body: Matter.Body
     graphics: PIXI.Graphics
-    radius: number
-    x: number
-    y: number
 
-    constructor(world: Matter.World, radius: number, x: number, y: number) {
+    constructor(world: Matter.World, body: Matter.Body, graphics: PIXI.Graphics) {
         this.world = world
-        this.radius = radius
-        this.x = x
-        this.y = y
 
-        this.body = Matter.Bodies.circle(x, y, radius)
+        this.body = body
         Matter.World.addBody(this.world, this.body)
 
-        this.graphics = new PIXI.Graphics()
-        this.graphics.beginFill(0x00FF00)
-        this.graphics.drawCircle(0, 0, this.radius)
-        this.graphics.endFill()
+        this.graphics = graphics
+        this.graphics.position.set(this.body.position.x, this.body.position.y)
+    }
 
-        this.graphics.position.set(this.x, this.y)
+    get x(): number {
+        return  this.body.position.x
+    }
+
+    get y(): number {
+        return  this.body.position.y
     }
 
     addTo(parent: PIXI.Container) {
@@ -36,9 +34,42 @@ class Orb {
     }
 
     update() {
-        this.x = this.body.position.x
-        this.y = this.body.position.y
-        this.graphics.position.set(this.x, this.y)
+        this.graphics.position.set(this.body.position.x, this.body.position.y)
+    }
+}
+
+class Barrier extends PhysicsObject {
+    width: number
+    height: number
+
+    constructor(world: Matter.World, x: number, y: number, width: number, height: number) {
+        let body = Matter.Bodies.rectangle(x, y, width, height, {isStatic: true})
+
+        let graphics = new PIXI.Graphics()
+        graphics.beginFill(0xFF0000)
+        graphics.drawRect(0, 0, width, height)
+        graphics.endFill()
+        graphics.pivot.set(width / 2, height / 2)
+
+        super(world, body, graphics)
+        this.width = width
+        this.height = height
+    }
+}
+
+class Orb extends PhysicsObject {
+    radius: number
+
+    constructor(world: Matter.World, x: number, y: number, radius: number) {
+        let body = Matter.Bodies.circle(x, y, radius)
+
+        let graphics = new PIXI.Graphics()
+        graphics.beginFill(0x00FF00)
+        graphics.drawCircle(0, 0, radius)
+        graphics.endFill()
+
+        super(world, body, graphics)
+        this.radius = radius
     }
 }
 
@@ -49,24 +80,14 @@ app.stage.addChild(FPS)
 let engine = Matter.Engine.create()
 let world = engine.world
 
-let slab = Matter.Bodies.rectangle(300, 300, 500, 25, {isStatic: true})
-Matter.World.add(world, slab)
+let slab = new Barrier(world, 300, 450, 500, 25)
+slab.addTo(app.stage)
 
-let rect = new PIXI.Graphics()
-rect.beginFill(0xFF0000)
-rect.drawRect(0, 0, 500, 25)
-rect.endFill()
-rect.pivot.set(250, 12.5)
+let orbs: Array<Orb> = []
 
-app.stage.addChild(rect)
-
-let orb = new Orb(world, 20, 300, 50)
-orb.addTo(app.stage)
-
-// Create the sprite and add it to the stage
-let sprite = new PIXI.Text("@") // PIXI.Sprite.from('sample.png');
-app.stage.addChild(sprite)
-
+let firstOrb = new Orb(world, 300, 50, 20)
+firstOrb.addTo(app.stage)
+orbs.push(firstOrb)
 
 let intervalsTotal: Array<number> = []
 let intervalsWork: Array<number> = []
@@ -75,6 +96,7 @@ let lastWork = 0
 
 // Add a ticker callback to move the sprite back and forth
 let lastStep = 0
+let lastTick = 0
 let elapsed = 0.0;
 
 // delta is in frames, not ms =()
@@ -100,10 +122,18 @@ function update(delta: number) {
         Matter.Engine.update(engine, 20)
     }
 
-    rect.position.set(slab.position.x, slab.position.y)
-    orb.update()
+    if (elapsed - lastTick >= 1000) {
+        lastTick = Math.floor(elapsed)
+        if (orbs.length < 10) {
+            let newOrb = new Orb(world, 300, 50, 20)
+            newOrb.addTo(app.stage)
+            orbs.push(newOrb)}
+    }
 
-    sprite.x = 100.0 + Math.cos(elapsed/50.0) * 100.0;
+    slab.update()
+    for (let orb of orbs) {
+        orb.update()
+    }
     
     let endWork = performance.now()
 
